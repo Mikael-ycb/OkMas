552399c03f1d7266009ab87e869999ec84fa938b
Mikael <mikaelel902@gmail.com>
2025-10-28 22:40:04 +0700

    memperbaiki header dan navbar

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,57 @@
 <header class="fixed top-0 left-0 w-full z-50 bg-white shadow after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10">
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $slot }}</h1>
+    <div class="max-w-7xl mx-auto flex items-center justify-between py-3 px-6">
+
+        <!-- Logo dan Nama -->
+        <div class="flex items-center space-x-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
+                alt="Logo OKMAS"
+                class="h-12 w-12 object-contain">
+            <h1 class="text-2xl font-bold">
+                <span class="text-blue-900">OK</span><span class="text-sky-500">MAS</span>
+            </h1>
+        </div>
+
+        <!-- Kontak -->
+        <div class="flex space-x-12 text-sm text-blue-900">
+
+            <!-- Telepon -->
+            <div class="flex items-center space-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-sky-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.494a1 1 0 01-.272 1.012L9.12 10.88a16 16 0 006.001 6.001l1.69-1.33a1 1 0 011.012-.272l4.494 1.498A1 1 0 0122 19.72V23a2 2 0 01-2 2h-1C9.715 25 3 18.285 3 10V9a2 2 0 012-2z" />
+                </svg>
+                <div>
+                    <p class="font-semibold text-xs uppercase tracking-wide">Telpon</p>
+                    <a href="" class="text-sky-500 font-medium">(0293)5921484</a>
+                </div>
+            </div>
+
+            <!-- Email -->
+            <div class="flex items-center space-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-sky-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M16 12H8m8-4H8m8 8H8m2-12h4a2 2 0 012 2v16a2 2 0 01-2 2H10a2 2 0 01-2-2V4a2 2 0 012-2z" />
+                </svg>
+                <div>
+                    <p class="font-semibold text-xs uppercase tracking-wide">Email</p>
+                    <a href="" class="text-sky-500 font-medium">puskescdrt@gmail.com</a>
+                </div>
+            </div>
+
+            <!-- Alamat -->
+            <div class="flex items-center space-x-2">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-sky-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M12 11c1.657 0 3-1.343 3-3S13.657 5 12 5 9 6.343 9 8s1.343 3 3 3zm0 0c-4.418 0-8 1.79-8 4v3h16v-3c0-2.21-3.582-4-8-4z" />
+                </svg>
+                <div>
+                    <p class="font-semibold text-xs uppercase tracking-wide">Alamat</p>
+                    <a href="" class="text-sky-500 font-medium">Jln. Pesanggrahan No 2, Ds. Candiroto <br>
+                        Kec. Candiroto, Kab. Temanggung</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
     </div>
 </header>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,9 +2,6 @@
             <div class=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div class="flex h-16 items-center justify-between">
                 <div class="flex items-center">
-                    <div class="shrink-0">
-                        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="size-8" />
-                    </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
